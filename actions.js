001a48f805862155ef10002640c2152a176122da
SebastianBukowski <seb-27@o2.pl>
2019-03-19 09:44:14 +0100

    Add shares

---
+++ actions.js
@@ -0,0 +1,46 @@
+import uuid from 'uuid';
+
+const ADD_COMMENT = 'ADD_COMMENT',
+	EDIT_COMMENT = 'EDIT_COMMENT',
+	REMOVE_COMMENT = 'DELETE_COMMENT',
+	THUMB_UP_COMMENT = 'THUMB_UP_COMMENT',
+	THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+	
+	
+
+function addComment(text){
+	return {
+	type: ADD_COMMENT,
+	text,
+	id: uuid.v4() 
+	}
+}
+
+function editComment(text, id){
+	return {
+		type: EDIT_COMMENT,
+		text,
+		id
+	}
+}
+
+function removeComment(id){
+	return {
+		type: REMOVE_COMMENT,
+		id
+	}
+}
+
+function thumbUpComment(id) {
+	return {
+		type: THUMB_UP_COMMENT,
+		id
+    }
+}
+
+function thumbDownComment(id) {
+	return {
+		type: THUMB_DOWN_COMMENT,
+		id
+    }
+}
